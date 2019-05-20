a11b0a8ccc62fadfdc90498b73f6133264d54021
Clare Lu <lu.clare@inventec.com>
2019-05-20 14:50:20 +0800

    Add 1 to switch mac addr (eth0 mac addr + 1)

--- dockers/docker-orchagent/orchagent.sh
+++ dockers/docker-orchagent/orchagent.sh
@@ -4,7 +4,8 @@
 # vendor specific code.
 export platform=`sonic-cfggen -y /etc/sonic/sonic_version.yml -v asic_type`
 
-MAC_ADDRESS=`ip link show eth0 | grep ether | awk '{print $2}'`
+#MAC_ADDRESS=`ip link show eth0 | grep ether | awk '{print $2}'`
+MAC_ADDRESS=$(sonic-cfggen -H -v 'DEVICE_METADATA.localhost.mac')
 
 # Create a folder for SwSS record files
 mkdir -p /var/log/swss
